9438b0cafb06cc38724cedfbcfa8f71a7ec50ff2
Benitez Fernando <49913532+benitezfernando@users.noreply.github.com>
2020-07-30 16:01:06 -0300

    commit

--- servidor/controladores/controlador.js
+++ servidor/controladores/controlador.js
@@ -2,15 +2,85 @@ var con = require("../lib/conexionbd");
 
 module.exports = {
   todosLosPosts(req, res) {
-   
-
-    let query = "SELECT * FROM posts";
-    
-
-    
-
-    
+    con.query("SELECT * FROM post", (err, rows, fields) => {
+      if (!err) {
+        res.json(rows);
+      } else {
+        console.log("no funciona la base de datos");
+      }
+    });
   },
 
- };
+  postId(req, res) {
+    con.query(
+      "SELECT * FROM post where post.id = ?",
+      [req.params.id],
+      (err, rows, fields) => {
+        if (!err) {
+          res.json(rows[0]);
+        } else {
+          console.log("no funciona la base de datos");
+        }
+      }
+    );
+  },
 
+  newPost(req, res) {
+    const { title, text } = req.body;
+    console.log(req.body);
+
+    con.query(
+      "INSERT INTO post (title, text) VALUES (?, ?)",
+      [title, text],
+      (err, rows, fields) => {
+        if (!err) {
+          res.json(rows);
+        } else {
+          console.log("no funciona la base de datos");
+        }
+      }
+    );
+  },
+
+  updatePost(req, res) {
+    const { id, title, text } = req.body;
+    con.query(
+      'UPDATE post SET title = "' +
+        title +
+        '", text = "' +
+        text +
+        '" WHERE id = ' +
+        id
+    ),
+      (err, rows, fields) => {
+        if (!err) {
+          res.json({ Status: "Guardado correctamente" });
+        } else {
+          console.log("no funciona la base de datos");
+        }
+      };
+  },
+
+  postDelete(req, res) {
+    con.query(
+      "DELETE FROM post WHERE id = " + req.params.id,
+      (err, rows, fields) => {
+        if (!err) {
+          res.json(rows);
+        } else {
+          console.log("no funciona la base de datos");
+        }
+      }
+    );
+  },
+
+  allDelete(req, res) {
+    con.query("DELETE FROM post", (err, rows, fields) => {
+      if (!err) {
+        res.json(rows);
+      } else {
+        console.log("no funciona la base de datos");
+      }
+    });
+  },
+};
--- servidor/lib/conexionbd.js
+++ servidor/lib/conexionbd.js
@@ -1,12 +1,20 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host     : "localhost",
-  port     : "3306",
-  user     : "root",
-  password : "1234567890",
-  database : "blogs"
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "password",
+  database: "blogs",
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log("Db connected");
+  }
 });
 
 module.exports = connection;
-
